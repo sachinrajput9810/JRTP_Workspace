8051855f7e293e0e00a1cf7c3c6277904c2af670
sachinrajput9810 <rajputsachin9810@gmail.com>
2026-01-15 13:47:06 +0530

    added first mini user mgmt project

---
+++ 01_Mini_Project_User_Mgmt_App/src/test/java/com/jrtp/__Mini_Project_User_Mgmt_App/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrtp.__Mini_Project_User_Mgmt_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
